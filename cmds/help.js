cc491006ed15df66c52a110378854184e679eb32
SHAWXDS <163335248+SHAWXDS@users.noreply.github.com>
2024-06-22 15:18:54 +0500

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -4,16 +4,15 @@ module.exports = {
     credits: "SHAW",
     cooldown: 16,		
     execute(api, event, args, commands) {
-        let helpMessage = '𝙷𝚒! 𝚃𝚑𝚒𝚜 𝙱𝚘𝚝 𝚒𝚜 𝚌𝚛𝚎𝚊𝚝𝚎𝚍 by Shahzaib Khanzada 𝚊𝚝 https://shaw-don.replit.app/. 𝙷𝚎𝚛𝚎 𝚊𝚛𝚎 𝚊𝚕𝚕 𝚝𝚑𝚎 𝚌𝚘𝚖𝚖𝚊𝚗𝚍𝚜 𝚒𝚗𝚜𝚝𝚊𝚕𝚕𝚎𝚍 𝚘𝚗 𝚝𝚑𝚒𝚜 𝚜𝚎𝚛𝚟𝚎𝚛\n';
-        helpMessage += '💮═══════════════💮\n';
+        let helpMessage = '𝙷𝚒! 𝚃𝚑𝚒𝚜 AI 𝚒𝚜 𝚌𝚛𝚎𝚊𝚝𝚎𝚍 by Shahzaib Khanzada 𝚊𝚝 https://shaw-don.replit.app/. 𝙷𝚎𝚛𝚎 𝚊𝚛𝚎 𝚊𝚕𝚕 𝚝𝚑𝚎 𝚌𝚘𝚖𝚖𝚊𝚗𝚍𝚜 𝚒𝚗𝚜𝚝𝚊𝚕𝚕𝚎𝚍 𝚘𝚗 𝚝𝚑𝚒𝚜 𝚜𝚎𝚛𝚟𝚎𝚛\n';
+        helpMessage += '🔴═══════════════🔴\n';
         commands.forEach((command, name) => {
             helpMessage += `𝙽𝚊𝚖𝚎: ${name}\n`;
             helpMessage += `𝙳𝚎𝚜𝚌𝚛𝚒𝚙𝚝𝚒𝚘𝚗: ${command.description || 'No description provided'}\n`;
             helpMessage += `𝚁𝚘𝚕𝚎: ${command.role || 'user'}\n`;
-            helpMessage += `Credits: ${command.credits || 'YAFB Community'}\n`;
             helpMessage += `Costs: ${command.coins !== undefined ? command.coins : 'Free'}\n`;
             helpMessage += `Cooldown: ${command.cooldown !== undefined ? command.cooldown : 'No cooldown'}\n`;
-            helpMessage += '💮═══════════════💮\n';
+            helpMessage += '🔴═══════════════🔴\n';
         });
         helpMessage += '💬https://facebook.com/1753855074';
         api.sendMessage(helpMessage, event.threadID);
