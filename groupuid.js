152eabda5e10de6e0d9d472b0a1fd4af9431786e
SHAWXDS <163335248+SHAWXDS@users.noreply.github.com>
2024-06-22 20:59:46 +0500

    Update groupuid.js

--- groupuid.js
+++ groupuid.js
@@ -2,7 +2,7 @@ const request = require('request');
 const fs = require('fs');
 const path = require('path');
 module.exports.config = {
-  name: "uid",
+  name: "groupuid",
   version: "1.0.0",
   role: 0,
   hasPrefix: true,
